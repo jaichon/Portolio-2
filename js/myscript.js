2b3d34164122a52ea008b6d7ceb702cf96fc3007
jaichon25 <ichonjhunior@gmail.com>
2018-07-22 16:45:11 +0800

    added portfolio section and polished styling

--- js/myscript.js
+++ js/myscript.js
@@ -46,5 +46,44 @@ $(document).ready(function(){
 	$('.carousel').carousel({
 		interval: 10000
 	})
+
+	//portfolio gallery
+	$(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+		// $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+		// $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+		$(".filter").not('.'+value).hide('3000');
+		$('.filter').filter('.'+value).show('3000');
+				
+			}
+		});
+			
+		if ($(".filter-button").removeClass("active")) {
+		$(this).removeClass("active");
+		}
+		$(this).addClass("active");
+
+
+		//Modal
+		// $('.modalphotos img').on('click', function() {
+		// 	$('#modal').modal( {
+		// 		show: true,
+		// 	})
+
+		// 	var mysrc = $(this).attr('src');
+		// 	$('#modalimage').attr('src', mysrc);
+		// });
+
+		// $('#modalimage').on('click', function() {
+		// 	$('#modal').modal('hide');
+		// });
 });
 
